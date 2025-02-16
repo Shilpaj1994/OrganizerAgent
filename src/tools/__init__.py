b0f70ab9684f2d075bceb3427402c1a159a0e24c
Shilpaj <shilpajbhalerao@gmail.com>
2025-02-16 14:28:06 +0530

    Refactor: Removed manually extracting information from todo.txt

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,3 +1,3 @@
 from tools.tool_schema import create_schema
-from tools.system_tools.system_tools import get_directory_name, scan_directory, identify_file_types, organize_files_by_type, extract_info_from_todo
+from tools.system_tools.system_tools import get_directory_name, scan_directory, identify_file_types, organize_files_by_type
 from tools.internet_tools.internet_tools import compress_image, compress_pdf, send_email, add_calendar_event, schedule_mail
